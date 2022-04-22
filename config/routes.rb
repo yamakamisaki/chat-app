200df27eae2f0732f8fff0c7e44da7156e42680e
yamakamisaki <s.chuuu02@gmail.com>
2022-04-22 16:56:12 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "rooms#index"
+  
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+     resources :messages, only: [:index, :create]
+  end
 end
